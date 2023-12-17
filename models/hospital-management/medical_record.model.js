9d70868297216b4e73a1cfadf5ebbf3859734a40
arjunbector <arjunbector@gmail.com>
2023-12-17 18:22:39 +0530

    added medical-record model

--- models/hospital-management/medical_record.model.js
+++ models/hospital-management/medical_record.model.js
@@ -1,3 +1,16 @@
-import mongoose, { mongo } from "mongoose";
-const medicalRecordSchema = new mongoose.Schema({},{timestamps: true});
+import mongoose from "mongoose";
+const medicalRecordSchema = new mongoose.Schema({
+    patient: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Patient"
+    },
+    doctor: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Doctor"
+    },
+    hospital: {
+        type: mongoose.Schema.Types.ObjectID,
+        ref: "Hospital"
+    }
+},{timestamps: true});
 export const MedicalRecord = mongoose.model("MedicalRecord", medicalRecordSchema);
